b1233fe9022b497df8fb9ada86b3ce2e37aab2c9
Guilherme Pimentel Marcolino <framework08@localiza.com>
2023-08-09 17:09:19 -0300

    Implementar menu com opções

---
+++ Domain/Constantes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public static class Constantes
+    {
+        public static readonly Dictionary<string, string> Opcoes = new Dictionary<string, string>()
+            {
+                { "1", "Depósito" },
+                { "2", "Saque" },
+                { "3", "Sair" }
+            };
+    }
+}
--- banco_framework/Program.cs
+++ banco_framework/Program.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Model;
+﻿using Domain;
+using Domain.Model;
 
 internal class Program
 {
@@ -9,6 +10,7 @@ internal class Program
         Console.WriteLine("Por favor, identifique-se");
         Console.WriteLine("");
         var pessoa = Identificacao();
+        ExibirMenu(pessoa);
     }
 
     static Pessoa Identificacao()
@@ -24,10 +26,38 @@ internal class Program
         Console.WriteLine("Seu CPF:");
         pessoa.Cpf = Console.ReadLine();
         Console.Clear();
-
-        Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
-        Console.ReadKey();
         
         return pessoa;
+    }
+
+    static void ExibirMenu(Pessoa pessoa)
+    {
+        var opcaoSelecionada = SelecionarOpcao(pessoa);
+
+        if (opcaoSelecionada == "3")
+            return;
+
+        Console.WriteLine(Constantes.Opcoes.Where(x => x.Key == opcaoSelecionada).First().Value);
+    }
+
+    static string SelecionarOpcao(Pessoa pessoa)
+    {
+        var opcaoSelecionada = "";
+
+        while (!Constantes.Opcoes.ContainsKey(opcaoSelecionada))
+        {
+            Console.Clear();
+            Console.WriteLine($"Como posso ajudar {pessoa.Nome}?");
+
+            foreach (var opcao in Constantes.Opcoes)
+                Console.WriteLine($"{opcao.Key} - {opcao.Value}");
+
+            Console.WriteLine("----------");
+            Console.WriteLine("Selecione uma opção:");
+
+            opcaoSelecionada = Console.ReadLine();
+        }
+
+        return opcaoSelecionada;
     }
 }
